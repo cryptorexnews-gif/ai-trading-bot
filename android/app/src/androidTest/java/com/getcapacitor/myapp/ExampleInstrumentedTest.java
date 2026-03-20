edb9bbb014713383e5119721504cf3364ae3ba2c
[dyad] <cryptorexnews@gmail.com>
2026-03-20 04:52:26 +0100

    [dyad] wrote 3 file(s)

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.getcapacitor.myapp;
+package com.getcapacitor.app;
 
 import static org.junit.Assert.*;
 
---
+++ android/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.getcapacitor.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
